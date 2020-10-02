ae977f6f416b2befb44a2ebdf7e47e902e4ce295
Papp Richárd <papprichard97@gmail.com>
2020-10-02 14:27:47 +0200

    add controller to stock and  create MainController.py

--- MainController.py
+++ MainController.py
@@ -3,17 +3,13 @@ import HTMLTestRunner
 import os
 import unittest
 from selenium import webdriver
-#from Teszt.UnitTestCase1 import Test
-from Teszt.UnitTestCase2 import Test2
-from TestCase1.Raktarak import Test
+from stock.StockController import StockController
 
 dir = os.getcwd()
 driver = webdriver
 
 # get all tests from SearchText and HomePageTest class
 warehouses = unittest.TestLoader().loadTestsFromTestCase(Test)
-#test1 = unittest.TestLoader().loadTestsFromTestCase(Test)
-#test2 = unittest.TestLoader().loadTestsFromTestCase(Test2)
 
 # create a test suite combining search_text and home_page_test
 test_suite = unittest.TestSuite([warehouses])
