313e7d9b5276f9925e0eea5d5bceab110630bc65
Papp Richárd <papprichard97@gmail.com>
2020-11-04 09:40:11 +0100

    change barchecking to read data from stocks

--- stock/BarCheckings.py
+++ stock/BarCheckings.py
@@ -17,19 +17,6 @@ class BarCheckings(BaseTestCase):
     def tearDownClass(self):
         super().tearDownClass()
 
-    def createBarChecking(self):
-        self.html.clickElement('Új standellenőrzés', 'a')
-        self.html.switchFrame('iframe')
-
-        self.html.clickElement('Pult', 'label', Options(following='label'))
-        self.html.clickElement('Indít', waitSeconds=3)
-
-        qty = int(self.html.getInput('barcheckItemValue', 'class').get_attribute('value'))-5
-        self.html.fillInput('barcheckItemValue', str(qty), 'class')
-        self.html.clickElement('Coca Cola 0.5 l', 'td', Options(following='button'))
-
-        self.html.clickElement('Lezárás', 'a')
-        self.html.refresh()
 
     def deleteChecking(self):
         self.html.clickTableElement('barchecking', 'id', 'Admin Admin', 'a', 'Törlés')
@@ -37,7 +24,19 @@ class BarCheckings(BaseTestCase):
         self.html.refresh()
 
     def testCreate(self):
-        self.createBarChecking()
+        self.html.clickElement('Új standellenőrzés', 'a')
+        self.html.switchFrame('iframe')
+
+        self.html.clickElement('Pult', 'label', Options(following='label'))
+        self.html.clickElement('Indít', waitSeconds=3)
+
+        qty = int(self.html.getInput('barcheckItemValue', 'class').get_attribute('value'))
+        modqty= qty-5
+        self.html.fillInput('barcheckItemValue', str(modqty), 'class')
+        self.html.clickElement('Coca Cola 0.5 l', 'td', Options(following='button'))
+
+        self.html.clickElement('Lezárás', 'a')
+        self.html.refresh()
 
         self.html.clickTableElement('barchecking', 'id', 'Admin Admin', 'a', 'Megtekintés')
 
@@ -52,10 +51,10 @@ class BarCheckings(BaseTestCase):
         self.html.clickElement('Mégsem')
         self.html.switchFrame()
 
-        self.stockAssert.assertStock('Coca Cola 0.5 l', 'Pult', '440')
+        self.stockAssert.assertStock('Coca Cola 0.5 l', 'Pult', str(modqty))
         self.html.clickElement('Standellenőrzések', 'a')
 
         self.deleteChecking()
 
-        self.stockAssert.assertStock('Coca Cola 0.5 l', 'Pult', '445')
+        self.stockAssert.assertStock('Coca Cola 0.5 l', 'Pult', str(qty))
 
