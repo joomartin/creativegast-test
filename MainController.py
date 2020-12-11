ceb4d8d735abe3ecaaf8f361810cd1ec8abb67f9
Richárd Papp <papprichard97@gmail.com>
2020-12-11 13:58:41 +0100

    main

--- MainController.py
+++ MainController.py
@@ -1,8 +1,11 @@
 from stock.StockController import StockController
 from products.ProductController import ProductController
+from receiving.ReceivingController import ReceivingController
 
 sc = StockController()
 pc = ProductController()
+rc= ReceivingController()
 
-sc.run()
-pc.run()
+#sc.run()
+#pc.run()
+rc.run()
