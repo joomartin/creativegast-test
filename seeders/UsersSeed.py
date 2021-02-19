e33dbcc9fb7e9401d21dffb8ca5cba51f038c1b1
Adrián Bán <pikapovwva@gmail.com>
2021-02-19 14:08:32 +0100

    add functions

--- seeders/UsersSeed.py
+++ seeders/UsersSeed.py
@@ -1,16 +1,21 @@
 from core.HtmlProxy import HtmlProxy
 from mainMenu.MainMenuProxy import MainMenuProxy
 from core.Options import Options
-from users.Users import UserAssert
+from users.UsersAssert import UserAssert
 
 
-class StockSeed:
+class UsersSeed:
 
     def __init__(self, driver):
         self.driver = driver
         self.html = HtmlProxy(self.driver)
         self.menu = MainMenuProxy(self.driver)
         self.usersAssert = UserAssert(self.html, self.driver)
+
+    def createGroup(self, name):
+        page = self.html.getElement('tabs-groups', 'div', options=Options(htmlAttribute='id'))
+        self.html.fillInput('Név', name)
+        self.html.clickElement('Rögzít', options=Options(element=page))
 
     def deleteGroup(self, name):
         self.html.clickTableElement('groups', 'id', name, 'a', 'Törlés', 'Csoportok')
--- users/Groups.py
+++ users/Groups.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+
 from shared.BaseTestCase import BaseTestCase
 from shared.TestData import TestData as data
 from core.Options import Options
@@ -24,6 +26,14 @@ class DiscountCards(BaseTestCase):
 
     def testCreateGroup(self):
         name = data.Group['Felszolgáló2']['Name']
+
+        self.usersSeed.createGroup(name)
+        self.assertTrue(self.html.getElementInTable(name, 'groups', 'Csoportok').is_displayed())
+        self.usersSeed.deleteGroup(name)
+
+
+    def testUpdate(self):
+        name = data.Group['Felszolgáló2']['Name']
         enter = data.Group['Felszolgáló2']['Rights']['Enter']
         enter2 = data.Group['Felszolgáló2']['Rights']['Enter2']
         restaurant = data.Group['Felszolgáló2']['Rights']['Restaurant']
@@ -31,15 +41,44 @@ class DiscountCards(BaseTestCase):
         closeDay = data.Group['Felszolgáló2']['Rights']['CloseDay']
         printCloseDay = data.Group['Felszolgáló2']['Rights']['PrintCloseDay']
 
-        page = self.html.getElement('tabs-groups', 'div', options=Options(htmlAttribute='id'))
+        self.usersSeed.createGroup(name)
 
-        self.html.fillInput('Név', name)
-        self.html.clickElement('Rögzít', options=Options(element=page))
+        self.html.clickTableElement('groups', 'id', name, 'a', 'Szerkeszt', 'Csoportok')
+        self.html.clickElement('Jogok', 'a')
+        self.html.switchFrame('iframe')
+        self.html.clickElement('inside scrollableContent', 'div', options=Options(htmlAttribute='class'))
+        html = self.driver.find_element_by_tag_name('html')
+        appeared = False
+        self.html.scroll()
+        #self.driver.find_element_by_xpath('.//a[contains(.,"Gyártás")]').submit()
+        print(self.html.getElement('Gyártás', 'a').is_displayed())
+        '''
+        while appeared is False:
+            html.send_keys(Keys.DOWN)
+            appeared = self.html.getElement('Gyártás', 'a').is_displayed()
+            try:
+                self.html.clickElement('Gyártás', 'a')
+            except Exception as ex:
+                print(ex)
+            print(self.html.getElement('Gyártás', 'a').is_displayed())
+            self.html.wait(0.5)
+        '''
 
-        self.assertTrue(self.html.getElementInTable(name, 'groups', 'Csoportok').is_displayed())
-
-    def testEdit(self):
-        pass
+        '''
+        self.html.clickElement('inside scrollableContent', 'div', options=Options(htmlAttribute='class'))
+        element = self.html.getElement('Gyártás', 'a')
+        html = self.driver.find_element_by_tag_name('html')
+        html.send_keys(Keys.DOWN)
+        self.html.wait(2)
+        '''
+        '''
+        self.html.clickElement(enter, 'a')
+        self.html.clickElement(enter2, 'a')
+        self.html.clickElement(restaurant, 'a')
+        self.html.clickElement(openDay, 'a')
+        self.html.clickElement(closeDay, 'a')
+        self.html.clickElement(printCloseDay, 'a')
+        '''
 
     def testCreateGroupWithRights(self):
         pass
