bf6b15e123f0215deaeaed8fd1f710baed6cb15a
Adrián Bán <pikapovwva@gmail.com>
2021-04-16 15:40:49 +0200

    add headless chrome option

--- seeders/UsersSeed.py
+++ seeders/UsersSeed.py
@@ -24,6 +24,7 @@ class UsersSeed:
 
     def createUser(self, surname, firstName, userName, password, position, group):
         self.html.fillInput('Vezetéknév', surname)
+        #self.html.fillInput('Vezetéknévasdasd', surname)
         self.html.fillInput('Keresztnév', firstName)
         self.html.fillInput('Felhasználónév', userName)
         self.html.fillInput('Jelszó', password)
